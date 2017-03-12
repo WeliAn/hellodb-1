1a8f5de548ffd9fdb0633fd8aa8081a3341ba057
WeliAN <P76051373@mail.ncku.edu.tw>
2017-03-12 14:26:01 +0800

    test push

--- src/main/java/codes/chia7712/hellodb/admin/SimpleAdmin.java
+++ src/main/java/codes/chia7712/hellodb/admin/SimpleAdmin.java
@@ -3,6 +3,7 @@ package codes.chia7712.hellodb.admin;
 import codes.chia7712.hellodb.Table;
 import codes.chia7712.hellodb.data.Cell;
 import codes.chia7712.hellodb.data.CellComparator;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -18,8 +19,22 @@ import java.util.stream.Collectors;
 class SimpleAdmin implements Admin {
 
   private final ConcurrentMap<String, Table> tables = new ConcurrentSkipListMap<>();
+  private final static String TABLES_PATH = "welian/";
+  private final static String SLASH = "/";
+  File f;
 
-  SimpleAdmin(Properties prop) {
+  SimpleAdmin(Properties prop) throws IOException {
+    f = new File(TABLES_PATH);
+    if (f.exists()) {
+      String[] tablesDir = f.list();
+      for (String table : tablesDir) {
+        if (new File(TABLES_PATH + table).isDirectory()) {
+          tables.put(table, new SimpleTable(table));
+        }
+      }
+    } else {
+      f.mkdirs();
+    }
   }
 
   @Override
@@ -28,6 +43,8 @@ class SimpleAdmin implements Admin {
       throw new IOException(name + " exists");
     }
     tables.computeIfAbsent(name, SimpleTable::new);
+    f = new File(TABLES_PATH + name);
+    f.mkdir();
   }
 
   @Override
@@ -40,6 +57,8 @@ class SimpleAdmin implements Admin {
     if (tables.remove(name) == null) {
       throw new IOException(name + " not found");
     }
+    f = new File(TABLES_PATH + name);
+    f.delete();
   }
 
   @Override
@@ -58,7 +77,14 @@ class SimpleAdmin implements Admin {
 
   @Override
   public void close() throws IOException {
-    //nothing
+    tables.forEach((k, v) -> {
+      try {
+        v.close();
+      } catch (IOException ex) {
+        System.out.println("Table " + k + " close failed");
+      }
+    });
+    tables.clear();
   }
 
   private static class SimpleTable implements Table {
@@ -66,6 +92,7 @@ class SimpleAdmin implements Admin {
     private static final CellComparator CELL_COMPARATOR = new CellComparator();
     private final ConcurrentNavigableMap<Cell, Cell> data = new ConcurrentSkipListMap<>(CELL_COMPARATOR);
     private final String name;
+    private final Object wLock = new Object();
 
     SimpleTable(final String name) {
       this.name = name;
@@ -73,48 +100,96 @@ class SimpleAdmin implements Admin {
 
     @Override
     public boolean insert(Cell cell) throws IOException {
-      return data.put(cell, cell) != null;
+      synchronized (wLock) {
+        try {
+          wLock.wait();
+        } catch (InterruptedException ex) {
+        } finally {
+          wLock.notify();
+        }
+        return data.put(cell, cell) != null;
+      }
     }
 
     @Override
     public void delete(byte[] row) throws IOException {
-      Cell rowOnlyCell = Cell.createRowOnly(row);
-      for (Map.Entry<Cell, Cell> entry : data.tailMap(rowOnlyCell).entrySet()) {
-        if (CellComparator.compareRow(entry.getKey(), rowOnlyCell) != 0) {
-          return;
-        } else {
-          data.remove(entry.getKey());
+      synchronized (wLock) {
+        try {
+          wLock.wait();
+        } catch (InterruptedException ex) {
+        } finally {
+          wLock.notify();
+        }
+        Cell rowOnlyCell = Cell.createRowOnly(row);
+        for (Map.Entry<Cell, Cell> entry : data.tailMap(rowOnlyCell).entrySet()) {
+          if (CellComparator.compareRow(entry.getKey(), rowOnlyCell) != 0) {
+            return;
+          } else {
+            data.remove(entry.getKey());
+          }
         }
       }
     }
 
     @Override
     public Iterator<Cell> get(byte[] row) throws IOException {
-      Cell rowOnlyCell = Cell.createRowOnly(row);
-      List<Cell> rval = new ArrayList<>();
-      for (Map.Entry<Cell, Cell> entry : data.tailMap(rowOnlyCell).entrySet()) {
-        if (CellComparator.compareRow(entry.getKey(), rowOnlyCell) != 0) {
-          break;
-        } else {
-          rval.add(entry.getValue());
+      synchronized (wLock) {
+        try {
+          wLock.wait();
+        } catch (InterruptedException ex) {
+        } finally {
+          wLock.notify();
         }
+        Cell rowOnlyCell = Cell.createRowOnly(row);
+        List<Cell> rval = new ArrayList<>();
+        for (Map.Entry<Cell, Cell> entry : data.tailMap(rowOnlyCell).entrySet()) {
+          if (CellComparator.compareRow(entry.getKey(), rowOnlyCell) != 0) {
+            break;
+          } else {
+            rval.add(entry.getValue());
+          }
+        }
+        return rval.iterator();
       }
-      return rval.iterator();
     }
 
     @Override
     public Optional<Cell> get(byte[] row, byte[] column) throws IOException {
-      return Optional.ofNullable(data.get(Cell.createRowColumnOnly(row, column)));
+      synchronized (wLock) {
+        try {
+          wLock.wait();
+        } catch (InterruptedException ex) {
+        } finally {
+          wLock.notify();
+        }
+        return Optional.ofNullable(data.get(Cell.createRowColumnOnly(row, column)));
+      }
     }
 
     @Override
     public boolean delete(byte[] row, byte[] column) throws IOException {
-      return data.remove(Cell.createRowColumnOnly(row, column)) != null;
+      synchronized (wLock) {
+        try {
+          wLock.wait();
+        } catch (InterruptedException ex) {
+        } finally {
+          wLock.notify();
+        }
+        return data.remove(Cell.createRowColumnOnly(row, column)) != null;
+      }
     }
 
     @Override
     public boolean insertIfAbsent(Cell cell) throws IOException {
-      return data.putIfAbsent(cell, cell) == null;
+      synchronized (wLock) {
+        try {
+          wLock.wait();
+        } catch (InterruptedException ex) {
+        } finally {
+          wLock.notify();
+        }
+        return data.putIfAbsent(cell, cell) == null;
+      }
     }
 
     @Override
@@ -125,6 +200,10 @@ class SimpleAdmin implements Admin {
     @Override
     public String getName() {
       return name;
+    }
+    
+    private void writeToDisk(){
+      
     }
 
   }
